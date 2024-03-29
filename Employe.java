7c45205b109884a21670089159d1ea90464434c3
jyothi <01fe22bca226@kletech.ac.in>
2024-03-29 17:01:56 +0530

    Adding Employe information code

---
+++ Employe.java
@@ -0,0 +1,31 @@
+import java.text.ParseException;
+
+class Employee extends person1 {
+    String empId;
+    double salary;
+
+   public Employee(String name, String dateOfBirth, String empId, double salary) throws ParseException {
+       super(name, dateOfBirth);
+       this.empId = empId;
+       this.salary = salary;
+   }
+
+   public void displayEmployeeDetails() {
+       displayPersonName();
+       displayAge();
+       System.out.println("Employee ID: " + empId);
+       System.out.println("Salary: $" + salary);
+   }
+}
+
+class Test {
+   public static void main(String[] args) throws ParseException {
+       person1 person = new person1("John Doe", "1990-05-15");
+       person.displayPersonName();
+       person.displayAge();
+       Employee employee = new Employee("Jane Smith", "1985-08-25", "E12345", 75000.0);
+       employee.displayEmployeeDetails();
+   }
+}
+
+
